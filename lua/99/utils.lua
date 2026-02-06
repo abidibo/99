57f1ae67953ddaea47c779e033fd9145d8703864
abidibo <abidibo@gmail.com>
2026-02-06 10:22:44 +0100

    refactor: tmp dir creation

--- lua/99/utils.lua
+++ lua/99/utils.lua
@@ -4,9 +4,15 @@ local M = {}
 --- to make the _99_state have the project directory.
 --- @return string
 function M.random_file()
+  local cwd = vim.uv.cwd()
+  local tmp_dir = string.format("%s/tmp", cwd)
+
+  -- Ensure tmp directory exists
+  vim.fn.mkdir(tmp_dir, "p")
+
   return string.format(
-    "%s/tmp/99-%d",
-    vim.uv.cwd(),
+    "%s/99-%d",
+    tmp_dir,
     math.floor(math.random() * 10000)
   )
 end
